b810c49bd543f49b12756ca7f9aa216cf9fb9ca3
guyinatuxedo <ryancmeinke@gmail.com>
2017-02-22 00:09:35 -0500

    New 32 bit f4 challenge complete.

---
+++ fmt_str/f4/f4_secure.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	fun0();
+}
+
+void fun0()
+{
+	puts("Oh I almost forgot to mention, due to budget cuts we decided to get rud if tge exit. Just think of all of the security research we can do now!. And by we, I mean you.\n");
+	char buf0[100];
+	fgets(buf0, sizeof(buf0), stdin);
+	printf("%s", buf0);
+	fflush(buf0);
+}
+
+void fire_exit()
+{
+	printf("Oh look, a fire exit. That's why we are still underbugdget. Level Cleared \n");
+}
